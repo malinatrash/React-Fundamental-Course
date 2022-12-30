d6eb3c697e8fd620e2e10a979d6dfa306dbd572f
malinatrash <malinatrashh@gmail.com>
2022-12-30 17:41:47 +0800

    Прикрутил сервак, чтобы подсасывать оттуда фейк данные, добавил крутилку, которая крутится, пока фетчатся данные!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './styles/App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 const App = () => {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll();
+        setPosts(response);
+    })
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -35,7 +46,11 @@ const App = () => {
             filter={filter}
             setFilter={setFilter}
         />
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
+        {isPostLoading
+            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+            :
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
+        }
     </div>
   );
 }
