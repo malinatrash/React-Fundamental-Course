c183c5f24ef1f896ab4758d73fd34c64ea1301b7
malinatrash <malinatrashh@gmail.com>
2023-01-05 05:17:11 +0800

    Навел красоты в пагинаторе

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import '../src/styles/App.css';
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
-import cl from './components/UI/button/MyButton.module.css'
 import NavigationBar from "./components/navigationBar/NavigationBar";
 
 const App = () => {
     return(
         <div>
             <NavigationBar/>
+            <Posts/>
             <Routes>
+                {/*<Route path='/' element={<Posts/>}/>*/}
                 <Route path='/about' element={<About/>}/>
-                <Route path='/' element={<Posts/>}/>
             </Routes>
         </div>
     )
